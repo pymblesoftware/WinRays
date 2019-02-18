ef62a6136a1b53f8337472658904dad37c9a083a
Regan Russell <regan@pymblesoftware.com>
2019-02-19 08:33:24 +1100

    moved UI elements into package, added scene object and light source

---
+++ WinRays/WinRays/Model/Surface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinRays.Model
+{
+    class Surface
+    {
+
+        double Ambient;
+        double Diffuse;
+        double Brilliance;
+        double Specular;
+        double Roughness;
+        double Reflection;
+
+    }
+}
